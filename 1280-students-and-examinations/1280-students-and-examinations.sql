0e0dabf51c106b62639619ed9680c7fd23c95b2b
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-01-10 17:57:06 -0500

    Time: 866 ms (60.42%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -3,11 +3,14 @@
 
 -- num of times each stud attend each exam
 
-SELECT s.student_id, s.student_name, sub.subject_name, CASE WHEN j.attended_exams IS NULL THEN 0 ELSE j.attended_exams END as attended_exams
-FROM Students s CROSS JOIN Subjects sub
-LEFT JOIN(SELECT student_id, COUNT(student_id) as attended_exams, subject_name
-FROM Examinations
-GROUP BY student_id, subject_name
+SELECT s.student_id, s.student_name, sub.subject_name, 
+    CASE WHEN j.attended_exams IS NULL THEN 0 ELSE j.attended_exams END as attended_exams
+FROM Students s 
+CROSS JOIN Subjects sub
+LEFT JOIN(
+    SELECT student_id, COUNT(student_id) as attended_exams, subject_name
+    FROM Examinations
+    GROUP BY student_id, subject_name
 ) AS j
-ON s.student_id = j.student_id and sub.subject_name = j.subject_name
+ON s.student_id = j.student_id AND sub.subject_name = j.subject_name
 ORDER BY student_id, subject_name
