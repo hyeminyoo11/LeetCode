7df3776a72df70f566bd24b0dbcf1de3df278ce0
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-02-15 18:11:48 -0500

    Time: 102 ms (11.93%), Space: 14.8 MB (38.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+
+        s1 = ''.join(filter(str.isalnum, s)) 
+        s2 = s1.lower()
+        reverse = ''
+        
+        for n in s2:
+            reverse = n + reverse
+        
+        print(s2)
+        print(reverse)
+        return reverse == s2
