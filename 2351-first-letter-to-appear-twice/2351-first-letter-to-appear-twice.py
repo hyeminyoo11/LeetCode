9dadef24faeff281feb1b5bdf2a73eab827d2736
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-02-22 18:28:46 -0500

    Time: 32 ms (63.83%), Space: 13.9 MB (43.13%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,18 +1,10 @@
-# class Solution:
-#     def repeatedCharacter(self, s: str) -> str:
-        
-#         maps = defaultdict(int)
-        
-#         for n in range(len(s)):
-#             maps[n].append(s[n])
-            
-#             return maps
-
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        letters = set()
+        
+        res = []
+        
         for char in s:
-            if char in letters:
+            if char in res:
                 return char
             else:
-                letters.add(char)
+                res.append(char)
