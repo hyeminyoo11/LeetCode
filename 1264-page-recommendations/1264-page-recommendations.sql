1588a18b032f4dab03bba6fc88bc0ece02e90fac
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-03-31 21:26:49 -0400

    Time: 1174 ms (33.23%), Space: 0B (100.00%) - LeetHub

---
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+WITH cte AS (SELECT *
+FROM Friendship
+WHERE user1_id = 1 OR user2_id = 1)
+
+SELECT DISTINCT l.page_id AS recommended_page
+FROM cte c
+INNER JOIN Likes l ON l.user_id = c.user1_id OR l.user_id = c.user2_id
+WHERE l.page_id NOT IN (
+  SELECT page_id FROM Likes WHERE user_id = 1)
+ORDER BY recommended_page;
