9e9d61b105d7f1819eb96a539eb9016b468fcf04
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-03-31 21:29:16 -0400

    Time: 1066 ms (58.41%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -7,6 +7,4 @@ WHERE user1_id = 1 OR user2_id = 1)
 SELECT DISTINCT l.page_id AS recommended_page
 FROM cte c
 INNER JOIN Likes l ON l.user_id = c.user1_id OR l.user_id = c.user2_id
-WHERE l.page_id NOT IN (
-  SELECT page_id FROM Likes WHERE user_id = 1)
-ORDER BY recommended_page;
+WHERE l.page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1)
