fb78945aa7cc4a60eef3bceeff619f1f47122182
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-05-19 21:14:24 -0400

    Time: 960 ms (42.18%), Space: 0B (100.00%) - LeetHub

--- 0574-winning-candidate/0574-winning-candidate.sql
+++ 0574-winning-candidate/0574-winning-candidate.sql
@@ -8,10 +8,11 @@ FROM Vote v
 LEFT JOIN Candidate c
 ON v.candidateID = c.id) total
 GROUP BY candidateId
-ORDER BY vote_count DESC
-LIMIT 1)
+)
 
 SELECT c.name 
 FROM best b
 LEFT JOIN Candidate c
 ON c.id = b.candidateID
+ORDER BY vote_count DESC
+LIMIT 1
