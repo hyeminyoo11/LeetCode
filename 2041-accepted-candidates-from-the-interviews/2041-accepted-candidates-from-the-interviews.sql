240df02937d167eb5dbdcf3984f2150e3310135f
Hyemin Yoo <54518064+hyeminyoo11@users.noreply.github.com>
2023-06-30 20:32:22 -0400

    Time: 2479 ms (40.51%), Space: 0B (100.00%) - LeetHub

--- 2041-accepted-candidates-from-the-interviews/2041-accepted-candidates-from-the-interviews.sql
+++ 2041-accepted-candidates-from-the-interviews/2041-accepted-candidates-from-the-interviews.sql
@@ -1,18 +1,5 @@
 # Write your MySQL query statement below
 
-
-# WITH score_sum AS (SELECT interview_id, SUM(score) AS sum_score
-# FROM Rounds
-# GROUP BY interview_id)
-
-# SELECT c.candidate_id
-# FROM Candidates c
-# LEFT JOIN score_sum s
-# ON c.interview_id = s.interview_id
-# WHERE c.years_of_exp >=2 and s.sum_score > 15
-# ;
-
-
 SELECT c.candidate_id
 FROM Candidates c
 LEFT JOIN (SELECT interview_id, SUM(score) AS sum_score
